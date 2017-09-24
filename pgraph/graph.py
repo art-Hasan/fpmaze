28342b551c5b2e089b1b8bc6003f11c1c8379da9
art-Hasan <guseyin.gasimov@gmail.com>
2017-09-24 12:52:39 +0300

    simple graph data structure implementation

---
+++ pgraph/graph.py
@@ -0,0 +1,37 @@
+# pgraph/graph.py
+
+from .vertex import Vertex
+
+""" Class for representaiton graph """
+class Graph:
+	def __init__(self):
+		self.vertList = dict()
+		self.numVertices = 0
+
+	def addVertex(self, key):
+		self.numVertices += 1
+		newVertex = Vertex(key)
+		self.vertList[key] = newVertex
+		return newVertex
+
+	def getVertex(self, n):
+		if n in self.vertList:
+			return self.vertList[n]
+		else:
+			return None
+
+	def addEdge(self, f, t, cost=0):
+		if f not in self.vertList:
+			nv = self.addVertex(f)
+		if t not in self.vertList:
+			nv = self.addVertex(t)
+		self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+	def getVertices(self):
+		return self.vertList.keys()
+
+	def __iter__(self):
+		return iter(self.vertList.values())
+
+	def __contains__(self, n):
+		return n in self.vertList
